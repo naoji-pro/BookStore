d85544af1ab5be71329632de9d7483c1fb3782e0
naoji-pro <124571420+naoji-pro@users.noreply.github.com>
2023-04-14 21:08:09 +0900

    Add files via upload

---
+++ db.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+DATEBASE='datebase.db'
+def create_book_table():
+    con = sqlite3.connect(DATEBASE)
+    con.execute("CREATE TABLE IF NOT EXISTS  books (title, price, arrival_day)")
+    con.close()
